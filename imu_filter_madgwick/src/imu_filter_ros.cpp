c586a856b050544cc426be330ab17cd07c4c13a8
Tamaki Nishino <otamachan@gmail.com>
2024-04-26 09:17:45 +0900

    Fix format
    Signed-off-by: Tamaki Nishino <otamachan@gmail.com>

--- imu_filter_madgwick/src/imu_filter_ros.cpp
+++ imu_filter_madgwick/src/imu_filter_ros.cpp
@@ -543,15 +543,14 @@ void ImuFilterMadgwickRos::reconfigCallback(
 void ImuFilterMadgwickRos::checkTopicsTimerCallback()
 {
     if (use_mag_)
-        RCLCPP_WARN_STREAM(
-            get_logger(),
-            "Still waiting for data on topics "
-            << imu_subscriber_->getTopic() << "and "
-            << mag_subscriber_->getTopic() << "...");
-    else
         RCLCPP_WARN_STREAM(get_logger(),
-                           "Still waiting for data on topic "
-                           << imu_subscriber_->getTopic() << "...");
+                           "Still waiting for data on topics "
+                               << imu_subscriber_->getTopic() << "and "
+                               << mag_subscriber_->getTopic() << "...");
+    else
+        RCLCPP_WARN_STREAM(get_logger(), "Still waiting for data on topic "
+                                             << imu_subscriber_->getTopic()
+                                             << "...");
 }
 
 #include "rclcpp_components/register_node_macro.hpp"
